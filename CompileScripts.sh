67f68843cf6bc8b0063bf4dd12486b8c62d7d464
zrei <88964793+zrei@users.noreply.github.com>
2025-12-02 21:35:56 +0800

    Update bash script

--- CompileScripts.sh
+++ CompileScripts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Pass in the day number - it will find the cpp file, compile it, run it and pass in the input file
-echo This script compiles the cpp file and passes in the input file
+# Pass in the day number - it will find the cpp file, compile it, run it and pass in the input file. You can also pass in an optional second input to use a different input file
+echo This script compiles the cpp file and passes in the input file. Provide alternative input file as optional second input
 
 dayNumber="Day$1"
 echo Compiling $dayNumber 
@@ -13,7 +13,13 @@ g++ --std=c++20 $folderName/$cppFileName "fileRead.cpp" -o $folderName/$outFileN
 echo Done compiling
 
 echo Running $dayNumber
+if [ -n "$2" ]
+then
+inFileName="$2"
+else
 inFileName=$dayNumber.in
+fi
+echo Using input file $inFileName
 ./$folderName/$outFileName ./$folderName/$inFileName
 
 echo Done
